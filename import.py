18970110fc0031df810760a6491ffa13d73e5336
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-05-18 13:52:35 -0400

    updated import.py

--- import.py
+++ import.py
@@ -1,32 +1,38 @@
 import sys
 from cs50 import SQL
 
-def main():
+import sys
+import csv
+from cs50 import SQL
 
+def main():
     if len(sys.argv) != 2:
         print ('Incorrect number of command line arguments')
         return 1
 
     db = SQL("sqlite:///students.db")
+    try:
+        with open(sys.argv[1]) as csv_file:
+            students = csv.DictReader(csv_file)
 
-    sql_query = '''SELECT * FROM students
-                   WHERE house = ?
-                   ORDER BY last, first;'''
-    students = db.execute(sql_query,sys.argv[1])
+            for row in students:
+                student_names = row['name'].split()
+                if len(student_names) == 3:
+                    middle = student_names[1]
+                else:
+                    middle = None
+                first = student_names[0]
+                last = student_names[-1]
 
-    if len(students) == 0:
-        print ('No house by that name')
-        return 1
+                sql_insert = '''INSERT INTO students (first,middle,last,house,birth)
+                                VALUES (?, ?, ?, ?, ?)'''
+                db.execute(sql_insert, first,middle,last,row['house'],row['birth'])
+        return 0
 
-    for student in students:
-        first = student['first']
-        middle = "" if student['middle'] == None else student['middle']
-        last = student['last']
-        birth_year = student['birth']
-        print (f"{first} {middle} {last}, born {birth_year}")
-    return 0
+    except FileNotFoundError:
+        print ('The command line argument provided is not a valid file name in the current directory')
 
 if main() == 0:
-    print ('Your query was successful')
+    print ('Database has been successfully updated')
 else:
-    print ('Your query was unsuccessful')
+    print ('There was an error updating the database')
